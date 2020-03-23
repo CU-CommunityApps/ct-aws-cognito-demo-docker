7ec02b66ee73e0f8c22fbfb755d55f4feb816dd5
Paul Allen <pea1@cornell.edu>
2020-03-23 14:32:28 +0000

    cleanup code

--- README.md
+++ README.md
@@ -2,32 +2,38 @@
 
 A super simple Python Flask App deployed in a Docker Container
 
-## Pull the container from Docker Hub
+This application is used as part of a demo that shows how to configure AWS Application Load Balancer and AWS Cognito to use Azure AD and SAML as identity providers. See [Configure AWS Application Load Balancer to use Cognito and Azure AD (ADFS/OIDC)](https://confluence.cornell.edu/x/kxXEFg) (private to Cornell community).
+
+This image is automatically built and is available on Docker Hub at https://hub.docker.com/repository/docker/pauleallen/ct-aws-cognito-demo-docker.
+
+## Miscellaneous Commands
+
+### Pull the container from Docker Hub
 
 ```
 docker pull pauleallen/ct-aws-cognito-demo-docker:latest
 ```
 
-## Build the container
+### Build the container locally
 
 ```
 docker build -t ct-aws-cognito-demo-docker .
 ```
 
-## How to run the container on a server
+### Run the container on a server
 
-If you build it locally:
+If you built it locally:
 ```
- docker run --detach --restart always -p 80:5000 ct-aws-cognito-demo-docker:latest
+docker run --detach --restart always -p 80:5000 ct-aws-cognito-demo-docker:latest
 ```
 
 If you pulled it from Docker Hub:
 ```
- docker run --detach --restart always -p 80:5000 pauleallen/ct-aws-cognito-demo-docker:latest
+docker run --detach --restart always -p 80:5000 pauleallen/ct-aws-cognito-demo-docker:latest
 ```
 
 
-## How to run the container locally
+### How to run the container locally
 
 In this mode, you can hack on `app.py` and the server will reload it when changes are saved.
 
@@ -37,6 +43,6 @@ Ensure your current working directory contains `app.py` before executing this co
 docker run -it --rm -p 80:5000 -v $(pwd):/app ct-aws-cognito-demo-docker:latest
 ```
 
-## Caveats
+### Caveats
 
 This should NOT be used for production purposes.
--- app.py
+++ app.py
@@ -1,98 +1,62 @@
-# flask_web/app.py
-
+"""
+Simple Flask application to show Cognito JWT data
+"""
+import pprint
+import json
+import base64
+import requests
+import jwt
 import flask
 from flask import Flask
-import pprint
-from logzero import logger
-import jwt
-import requests
-import base64
-import json
-import pprint
 
 app = Flask(__name__)
 
-@app.route('/azure')
-def azure():
-  if 'X-Amzn-Oidc-Accesstoken' in flask.request.headers:
-    oidc_accesstoken = flask.request.headers['X-Amzn-Oidc-Accesstoken'] 
-  logger.debug("oidc_accesstoken")
-  logger.debug(oidc_accesstoken)
-  graph_data = requests.get(  # Use token to call downstream service
-   'https://graph.microsoft.com/v1.0/me?$select=userPrincipalName,id,onPremisesSecurityIdentifier',
-    headers={'Authorization': 'Bearer ' + oidc_accesstoken},
-    ).json()
-  logger.debug(graph_data)
-  return(graph_data)
-
-@app.route('/cognito')
-def cognito():
-  if 'X-Amzn-Oidc-Accesstoken' in flask.request.headers:
-    oidc_accesstoken = flask.request.headers['X-Amzn-Oidc-Accesstoken'] 
-  logger.debug("oidc_accesstoken")
-  logger.debug(oidc_accesstoken)
-
-  graph_data = requests.get(  # Use token to call downstream service
-   'https://pea1-kognito-oidc.auth.us-east-1.amazoncognito.com/oauth2/userInfo',
-    headers={'Authorization': 'Bearer ' + oidc_accesstoken},
-    ).json()
-  logger.debug(graph_data)
-  return(graph_data)
-
 @app.route('/')
 def index():
-  
-# https://docs.aws.amazon.com/elasticloadbalancing/latest/application/listener-authenticate-users.html  
-  oidc_accesstoken = "NOT_FOUND"
-  oidc_identity = "NOT_FOUND"
-  oidc_data = "NOT_FOUND"
-  payload = {}
-  if 'X-Amzn-Oidc-Accesstoken' in flask.request.headers:
-    oidc_accesstoken = flask.request.headers['X-Amzn-Oidc-Accesstoken']
-  
-  if 'X-Amzn-Oidc-Identity' in flask.request.headers:
-    oidc_identity = flask.request.headers['X-Amzn-Oidc-Identity']
+    """
+    Grab and decode data from request headers and ALB headers
+    """
 
-  if 'X-Amzn-Oidc-Data' in flask.request.headers:
-    oidc_data = flask.request.headers['X-Amzn-Oidc-Data']
-  
-    # Step 1: Get the key id from JWT headers (the kid field)
-    encoded_jwt = oidc_data
-    jwt_headers = encoded_jwt.split('.')[0]
-    decoded_jwt_headers = base64.b64decode(jwt_headers)
-    decoded_jwt_headers = decoded_jwt_headers.decode("utf-8")
-    decoded_json = json.loads(decoded_jwt_headers)
-    kid = decoded_json['kid']  
-    print("===============decoded_jwt_headers===============")
-    pprint.pprint(decoded_json)
-    print("=================================================")
-    
-    # Step 2: Get the public key from regional endpoint
-    region = 'us-east-1'
-    url = 'https://public-keys.auth.elb.' + region + '.amazonaws.com/' + kid
-    req = requests.get(url)
-    pub_key = req.text
-  
-    # Step 3: Get the payload
-    payload = jwt.decode(encoded_jwt, pub_key, algorithms=['ES256'])
-    print("===============payload===========================")    
-    pprint.pprint(payload)
-    print("=================================================")
-    
-  result = "<table border=1>"
-  headers = dict(flask.request.headers)
-  for h in headers:
-    result += "<tr><td>" + h + "</td><td>" + headers[h] + "</td></tr>"
-    
-  # result += "<tr><td><b>X-Amzn-Oidc-Identity</b></td><td>" + oidc_identity + "</td></tr>"
-  # result += "<tr><td><b>X-Amzn-Oidc-Data</b></td><td>" + oidc_data + "</td></tr>"
-  # result += "<tr><td><b>X-Amzn-Oidc-Accesstoken</b></td><td>" + oidc_accesstoken + "</td></tr>"
+    # https://docs.aws.amazon.com/elasticloadbalancing/latest/application/listener-authenticate-users.html
+    oidc_data = "NOT_FOUND"
+    payload = {}
 
-  for k in payload:
-    result += "<tr><td>JWT Payload: " + str(k) + "</td><td>" + str(payload[k]) + "</td></tr>"
+    if 'X-Amzn-Oidc-Data' in flask.request.headers:
+        oidc_data = flask.request.headers['X-Amzn-Oidc-Data']
 
-  result += "</table>"
-  return result
+        # Step 1: Get the key id from JWT headers (the kid field)
+        encoded_jwt = oidc_data
+        jwt_headers = encoded_jwt.split('.')[0]
+        decoded_jwt_headers = base64.b64decode(jwt_headers)
+        decoded_jwt_headers = decoded_jwt_headers.decode("utf-8")
+        decoded_json = json.loads(decoded_jwt_headers)
+        kid = decoded_json['kid']
+        print("===============decoded_jwt_headers===============")
+        pprint.pprint(decoded_json)
+        print("=================================================")
+
+        # Step 2: Get the public key from regional endpoint
+        region = 'us-east-1'
+        url = 'https://public-keys.auth.elb.' + region + '.amazonaws.com/' + kid
+        req = requests.get(url)
+        pub_key = req.text
+
+        # Step 3: Get the payload
+        payload = jwt.decode(encoded_jwt, pub_key, algorithms=['ES256'])
+        print("===============payload===========================")
+        pprint.pprint(payload)
+        print("=================================================")
+
+    result = "<table border=1>"
+    headers = dict(flask.request.headers)
+    for header in headers:
+        result += "<tr><td>"+header+"</td><td>"+headers[header]+"</td></tr>"
+
+    for key in payload:
+        result += "<tr><td>JWT Payload: "+str(key)+"</td><td>"+str(payload[key])+"</td></tr>"
+
+    result += "</table>"
+    return result
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
